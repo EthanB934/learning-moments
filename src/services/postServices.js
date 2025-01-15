2830ddf334b031c6255a13cd09eda924dddf8634
Ethan Brown <eb934053@gmail.com>
2025-01-15 15:36:33 -0600

    Added delete funcionality to My Posts component

--- src/services/postServices.js
+++ src/services/postServices.js
@@ -27,7 +27,7 @@ export const createNewPost = (postObject) => {
 }
 
 export const deleteMyPost = (postObjectId) => {
-    return fetch (`http://localhost:8088/posts/${postObjectId}`, {
+    return fetch (`http://localhost:8088/posts/${postObjectId.id}`, {
         method: "Delete"
-    })
+    }).then(getAllPosts())
 }
